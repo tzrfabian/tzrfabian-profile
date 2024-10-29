d88ded737a81a8e43f58b14463227ddeccf73c21
Fabian Tenzara <fabiantenzara@gmail.com>
2024-10-29 19:00:37 +0700

    feat: set layout for homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,29 @@
-import Navbar from "@/components/Navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-    aa
+<div className="mt-28 rounded-md px-4 py-12">
+  <div className="grid md:grid-cols-2 justify-between items-center max-w-5xl mx-auto">
+    <div className="max-md:text-center">
+      <h2 className="md:text-4xl text-slate-100 text-4xl font-semibold md:!leading-[50px] mb-2">
+        Hi there! 👋
+      </h2>
+      <h2 className="md:text-4xl text-slate-100 text-4xl font-semibold md:!leading-[50px]">
+        My Name is Fabian Tenzara
+      </h2>
+      <p className="text-gray-400">
+        Laboris qui Lorem ad tempor ut reprehenderit. Nostrud anim nulla officia
+        ea sit deserunt. Eu eu quis anim aute Laboris qui Lorem ad tempor ut
+        reprehenderit.
+      </p>
     </div>
+    <div>
+      <img
+        src="https://media.tenor.com/hVRhFeDFW6oAAAAi/anime-wave.gif"
+        alt="wave"
+        className="w-48 mx-auto"
+      />
+    </div>
+  </div>
+</div>
   );
 }
