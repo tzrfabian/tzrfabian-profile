0305c80d1a2ab41640409ab19cd15f76c38cb701
Fabian Tenzara <fabiantenzara@gmail.com>
2024-11-01 22:06:42 +0700

    refac: adjusting projects page & add data.json

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,22 +1,23 @@
 'use client'
 import ProjectCard from '@/components/ProjectCard'
-import React from 'react'
+import data from '@/data/data.json'
 
 export default function Projects() {
+  const projects = data.projects;
+
+  console.log(projects);
   return (
-      <div className='mt-16 rounded-md px-4 py-8'>
+      <div className='mt-3 rounded-md px-4 py-8'>
         <div className='max-md:text-center flex justify-center items-center'>
           <h2 className="md:text-4xl text-slate-100 text-xl font-bold md:!leading-[50px] mb-2 text-center">
             Lift of My Projects
           </h2>
         </div>
         <div className='items-center max-w-2xl mx-auto'>
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+          {projects.map((project) => {
+            return <ProjectCard key={project.id} project={project}/>
+          })}
+          
       </div>
     </div>
   )
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,29 @@
 'use client'
+import Link from 'next/link'
 import React from 'react'
 
-export default function ProjectCard() {
+export default function ProjectCard( {project} ) {
   return (
-    <div className='border border-white rounded-lg my-3 p-3 bg-gray-800 bg-opacity-80'>
-      <h2 className='text-xl font-semibold text-white'>Project Name</h2>
-      <p className='text-gray-200'>Project Description</p>
-      <p className='text-gray-200 text-sm'>Tech Stack: </p>
+    <div className='border border-white rounded-lg my-5 p-3 bg-gray-800 bg-opacity-80 hover:translate-x-3 transition-all duration-300'>
+      <h2 className='text-2xl font-bold text-white'>{project.name}</h2>
+      <p className='text-gray-200'>
+        {project.description || 'No description available'}
+      </p>
+      <p className='text-gray-200 mb-2'>
+        <span className='font-semibold'>Tech Stack: </span>
+        {project.techStack.join(', ')}
+      </p>
+      <Link href={project.repository}>
+        <p className='text-gray-200 font-semibold hover:underline hover:text-cyan-600'>Repository Link</p>
+      </Link>
+
+      {project.deployLink ? (
+        <Link href={project.deployLink}>
+          <p className='text-gray-200 font-semibold hover:underline hover:text-cyan-600'>Deploy Link</p>
+        </Link>
+      ) : (
+        <p className='text-gray-200 text-opacity-50'>{"<Not Deployed Yet>"}</p>
+      )}
     </div>
   )
 }
