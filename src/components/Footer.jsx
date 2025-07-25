220b587566e44ca49120c82abad6d681133f940b
Fabian Tenzara <fabiantenzara@gmail.com>
2025-07-25 09:52:44 +0700

    refac: minor update on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-    <div>
+    <div className="relative z-10">
         <footer className="bg-slate-800 text-gray-400 py-5">
         <div className="max-w-5xl mx-auto text-center">
             <p>&copy; 2024 Fabian Tenzara</p>
