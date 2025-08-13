8ad0acbb3546c8c2d4555ac8a8b49fa14b231a67
Fabian Tenzara <fabiantenzara@gmail.com>
2025-08-13 15:41:30 +0700

    refac: update position and scaling page

--- src/components/Intro1.jsx
+++ src/components/Intro1.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 export default function Intro1() {
   return (
     <div className='animate-slideinright'>
-    <section className="mt-28 rounded-md px-4 py-12">
+    <section className="lg:mt-28 mt-5 rounded-md px-4 py-12">
         <div className="grid md:grid-cols-2 justify-between items-center max-w-5xl mx-auto">
         <div className="max-md:text-center">
             <h2 className="md:text-4xl text-slate-100 text-4xl font-semibold md:!leading-[50px] mb-2">
             Hi there! 👋
             </h2>
-            <h2 className="md:text-4xl text-slate-100 text-4xl font-semibold md:!leading-[50px]">
+            <h2 className="md:text-4xl text-slate-100 text-3xl font-semibold md:!leading-[50px]">
             My Name is Fabian Tenzara
             </h2>
-            <p className="text-gray-400 text-xl">
+            <p className="text-gray-400 text-xl lg:mt-4 mt-2">
             I am very interested in Web, API & Automation development.
             </p>
             <p className="text-gray-400 text-xl">
--- src/components/Intro2.jsx
+++ src/components/Intro2.jsx
@@ -4,20 +4,28 @@ import Image from "next/image";
 export default function Intro2() {
   return (
     <div className='animate-slideinleft'>
-    <section id="about" className="mt-40 mb-20 rounded-md px-4 py-12">
+    <section id="about" className="lg:mt-48 mt-16 mb-10 rounded-md px-4 py-12">
         <div className="grid md:grid-cols-2 justify-between items-center max-w-5xl mx-auto">
         <div className="max-md:text-center">
             <h2 className="md:text-4xl text-slate-100 text-4xl font-semibold md:!leading-[50px] mb-1">
             About Me 👾
             </h2>
+              <hr className="border-gray-500 mb-4"/>
 
-            <hr className="border-gray-500 mb-4"/>
+            {/* Mobile image - only visible on small screens */}
+            <div className='block md:hidden mb-4'>
+                <Image
+                src={image}
+                alt="picture"
+                className="w-48 mx-auto my-3 bg-gray-500 rounded-full transition-transform transform hover:scale-105 duration-300"
+                />
+            </div>
             
             <p className="text-gray-400 text-lg">
             {"My fullname is Muhammad Habib Fabian Tenzara, you can call me Fabian. I am a computer science graduate from Bina Nusantara University. I previously had an internship experience as a DevOps Engineer for 1 year. And graduates from Hacktiv8's Full Stack Javascript intensive Bootcamp program. Now, currently working at PT Agrodana Futures as Internal Developer."}
             </p>
         </div>
-        <div>
+        <div className='hidden md:block'>
             <Image
             src={image}
             alt="picture"
